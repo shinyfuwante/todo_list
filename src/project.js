9a35fe90ae20374ac49b89fa1486ac238a49a4ce
shinyfuwante <mr.philliptang@gmail.com>
2022-05-04 22:54:38 -0700

    Extend project.js
    This commit creates more functionality for project.js such as remove, add, getObject, and contains.

--- src/project.js
+++ src/project.js
@@ -3,15 +3,42 @@ class projectList {
         this.list = list;
     }
 
+    get length() {
+        return this.list.length;
+    }
+
+    contains(todoObject) {
+        for (let i = 0; i < this.length; i++) {
+            const item = this.list[i];
+            if (item.toString() == todoObject.toString()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getObject(todoObject) {
+        if (this.contains(todoObject)) {
+            for (let i = 0; i < this.length; i++) {
+                const item = this.list[i];
+                if (item.toString() == todoObject.toString()) {
+                    return item;
+                }
+            }
+        }
+        console.log('Failed to retrieve object');
+    }
+
     addTodo(todoObject) {
         this.list.push(todoObject);
     }
     
     removeTodo(todoObject) {
-        for (let i = 0; i < this.list.size; i++) {
-            const item = list[i];
+        console.log(this.length);
+        for (let i = 0; i < (this.length); i++) {
+            const item = this.list[i];
             if (item.toString() == todoObject.toString()) {
-                list.splice(i, 1);
+                this.list.splice(i, 1);
             }
         }
     }
