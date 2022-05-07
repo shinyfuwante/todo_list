439de03247e4f3d5581c947543d469f073648ac0
shinyfuwante <mr.philliptang@gmail.com>
2022-05-06 22:16:02 -0700

    More code to test basic functionality of todoObjects

--- src/index.js
+++ src/index.js
@@ -19,3 +19,27 @@ projects.removeTodo(todo2);
 console.log(projects.contains(todo2));
 console.log(projects.getObject(todo1));
 console.log(projects);
+
+console.log(todo2);
+todo2.title = "Some other title";
+todo2.description = "Some description";
+todo2.dueDate = "Some Date";
+todo2.priority = "Sick";
+todo2.completed = !todo2.completed;
+console.log(todo2);
+
+const body = document.querySelector('body');
+console.log(body);
+const button = document.createElement('button');
+button.innerText = '+';
+body.appendChild(button);
+
+
+const createNewTodo = (e) => {
+    const newTodo = new todoItem("Something to do", "We gotta do something", "Tomorrow", "Highest");
+    console.log(newTodo);
+    projects.addTodo(newTodo);
+    console.log(projects);
+}
+
+button.addEventListener('click', createNewTodo);
